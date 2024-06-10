3d54eb8f386bb7278d441f6f3d293a1cf1ea769c
Alex1445085 <n1445085@yandex.ru>
2024-06-10 09:58:39 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println();
+
+        // 1.
+        System.out.println("1.");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
+
+
+
 }
